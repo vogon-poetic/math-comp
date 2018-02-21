0ceca89ba66b95256069d64d0cbfc18c52f8ef9b
John McCrary <johnmccrarywork@gmail.com>
2018-02-21 10:54:53 -0600

    nested loops

--- primes.R
+++ primes.R
@@ -2,14 +2,22 @@
 #input
 rm(list=ls())
 #number<-as.numeric(readline("Enter an integer number: "))
-number<-13*19
-prime<-F
 
-for (i in 2:(number-1)) {
-   if ((number %% i) == 0) {
-       prime<-F
-       break;
-   }
+response <- "Y"
+while(response == "Y" || response == "y") {
+    number<-as.numeric(readline("Enter a positive int > 2: "))
+    prime<-F
+    
+    for (i in 2:(number-1)) {
+       if ((number %% i) == 0) {
+           cat(sprintf("%d is not a prime number\n", number))
+           break;
+       }
+    }
+    
+    if (i == (number - 1)) {
+        cat(sprintf("%d is a prime number.\n", number))
+    }
+    
+    response<-readline("Do you want to run again? (Y/N): ") # control statemrnt
 }
-
-print(prime)
