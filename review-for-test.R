a0f67ac169cd7ddec154593aff18e8d227dbf182
vogon-poetic <johnmccrarywork@gmail.com>
2018-02-23 10:33:07 -0600

    review day

---
+++ review-for-test.R
@@ -0,0 +1,38 @@
+#Purpose: review sheet answers
+rm(list=ls())
+
+##Question 1) print first n fibonacci numbers
+#n <- as.numeric(readline("enter a positive int (n): "))
+#f <- numeric(n)
+#f[1] <- f[2] <- 1
+#for (i in 3:n) {
+#    f[i] <- f[i-1] + f[i-2]
+#}
+#
+#print(f)
+
+##Question 2) check if a five-digit number n is divisible by 9 
+n <- as.numeric(readline("enter a positive int (n): "))
+n_copy <- n
+digits <- numeric(5)
+for (i in 1:5) {
+    digits[i] <- n %%  10
+    n <- n %/% 10
+}
+
+if (sum(digits) %% 9 == 0) {
+    cat(sprintf("%d is divisible by 9\n", n_copy))
+} else {
+    cat(sprintf("%d is NOT divisible by 9\n", n_copy))
+}
+
+#Question 3) prompt the user to enter a non-negative number, and a 
+#           real number. approximate cos(x) and compare with builtin
+#n <- as.numeric(readline("enter a positive int (n): "))
+#x <- as.numeric(readline("enter a real number (x): "))
+#y <- cos(x)
+#p.x <- 0 # this is what our approximation will be stored in
+#
+#for (i in 0:n) {
+#    
+#}
