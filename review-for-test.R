61436967a0dda7d8a583818ff00dfbb05a3aadef
vogon-poetic <johnmccrarywork@gmail.com>
2018-02-27 10:47:56 -0600

    test review

--- review-for-test.R
+++ review-for-test.R
@@ -12,19 +12,19 @@ rm(list=ls())
 #print(f)
 
 ##Question 2) check if a five-digit number n is divisible by 9 
-n <- as.numeric(readline("enter a positive int (n): "))
-n_copy <- n
-digits <- numeric(5)
-for (i in 1:5) {
-    digits[i] <- n %%  10
-    n <- n %/% 10
-}
-
-if (sum(digits) %% 9 == 0) {
-    cat(sprintf("%d is divisible by 9\n", n_copy))
-} else {
-    cat(sprintf("%d is NOT divisible by 9\n", n_copy))
-}
+#n <- as.numeric(readline("enter a positive int (n): "))
+#n_copy <- n
+#digits <- numeric(5)
+#for (i in 1:5) {
+#    digits[i] <- n %%  10
+#    n <- n %/% 10
+#}
+#
+#if (sum(digits) %% 9 == 0) {
+#    cat(sprintf("%d is divisible by 9\n", n_copy))
+#} else {
+#    cat(sprintf("%d is NOT divisible by 9\n", n_copy))
+#}
 
 #Question 3) prompt the user to enter a non-negative number, and a 
 #           real number. approximate cos(x) and compare with builtin
@@ -36,3 +36,16 @@ if (sum(digits) %% 9 == 0) {
 #for (i in 0:n) {
 #    
 #}
+
+
+# Taylor series for sin(x)
+rm(list = ls())
+x <- as.numeric(readline("Enter (x): "))
+n < - as.numeric(readline("Enter (n): "))
+sin.x <- 0
+
+for (k in 0:n) {
+    sin.x <- sin.x + ((-1)^k / factorial(2*k + 1)) * x^(2*k + 1)
+    print(sin.x)
+}
+
