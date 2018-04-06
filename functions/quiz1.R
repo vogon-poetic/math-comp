08c5b87a5a0315b87373c69058b0de20896c03ae
vogon-poetic <johnmccrarywork@gmail.com>
2018-04-06 10:46:40 -0500

    ggplot2 practice

--- functions/quiz1.R
+++ functions/quiz1.R
@@ -2,7 +2,7 @@ SimulateCoin <- function(s,n) {
     set.seed(s)
     #0 = head, #1 = tail
     
-    outcome <- sample(0:1, n, replace = T, prob = c(.5, .5))
+    outcome <- sample(0:1, n, replace = T, prob = c(.7, .3))
     n.heads <- sum(outcome == 0)
     n.tails <- sum(outcome == 1)
     p.heads <- n.heads/n
