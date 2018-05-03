98e3aefb714b81579c88110fcae6a7b77eed660c
John McCrary <johnmccrarywork@gmail.com>
2018-05-03 13:51:02 -0500

    q2,3 lab2

--- modified_question2_and_3_lab2.R
+++ modified_question2_and_3_lab2.R
@@ -2,16 +2,17 @@
 #Purpose: Convert temperature based on unit
 rm(list=ls())
 #Input
-temp<-as.numeric(readline("Enter temperature: "))
 unit<-readline("Enter unit of temperature (F or C): ")
+temp<-as.numeric(readline("Enter temperature: "))
+
+celsius<-(temp - 32)*(5/9)
+fahrenheit<-(temp * (9/5)) + 32
 
 #Computation and Output
 if (unit == "F") {
-    celsius<-(temp - 32)*(5/9)
     cat(sprintf("%.2f degrees Fahrenheit = %.2f degrees Celsius", 
                 temp, celsius))
 } else if (unit == "C") {
-    fahrenheit<-(temp * (9/5)) + 32
     cat(sprintf("%0.2f degrees celsius = %0.2f degrees fahrenheit",
                 temp, fahrenheit))
 } else {
